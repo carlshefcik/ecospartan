715ad1009babb15e9c0a96a2d354d28929fdc60d
carlshefcik <32783344+carlshefcik@users.noreply.github.com>
2018-03-16 13:19:07 -0700

    Added Firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
